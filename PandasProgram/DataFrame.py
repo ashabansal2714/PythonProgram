5ca4bd52b7ec6c5e9f8328b04e62304700302e28
asha bansal <ashabansal2714@gmail.com>
2023-05-11 23:42:30 +1100

    Create a dictionary and dataframe through pandas

---
+++ PandasProgram/DataFrame.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+dict = {"Name":["Asha","Antima","Milky","Sunny"],
+        "Age":[28,30,32,26],
+        "Status":["single","married","married","single"]}
+df = pd.DataFrame(dict)
+print(df)
